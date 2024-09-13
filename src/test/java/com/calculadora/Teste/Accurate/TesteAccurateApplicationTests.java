032d56fb87749857d7a33c1cd43e957561d0bd35
Rafael Santos <109754987+rafaelduartejv@users.noreply.github.com>
2024-09-13 19:38:16 -0300

    Initial commit

---
+++ src/test/java/com/calculadora/Teste/Accurate/TesteAccurateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculadora.Teste.Accurate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteAccurateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
